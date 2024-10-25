ce0019b39ec578399af69c7f9a3580bb2f4915f5
andricks <aestradaa10@Miumg.edu.gt>
2024-10-25 12:06:53 -0600

    agregando middlewares de seguridad

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const dataRoutes = require('./routes/dataRoutes');
 const logger = require('./logger/loggerServer');
 
 const app = express();
 const PORT = process.env.PORT || 4001;
+
+app.use(helmet());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100 // limita cada IP a 100 solicitudes por ventana de 15 minutos
+});
+app.use(limiter);
 
 app.use(express.json());
 app.use('/api', dataRoutes);
