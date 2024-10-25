563c02f28dcd77f4dba92caa85aff63782b27384
andricks <aestradaa10@Miumg.edu.gt>
2024-10-25 11:53:34 -0600

    agregamos los loggers

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dataRoutes = require('./routes/dataRoutes');
+const logger = require('./logger/logger');
 
 const app = express();
 const PORT = process.env.PORT || 4001;
@@ -8,5 +9,5 @@ app.use(express.json());
 app.use('/api', dataRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  logger.info(`Servidor corriendo en http://localhost:${PORT}`);
 });
